92df8075f96a3149f73184e497c8f6ca269fce4e
devdigital <devdigital.cloud@gmail.com>
2017-01-19 13:19:55 +0000

    Added http response message extensions

--- WebApiTestServer/WebApiTestServer.Api.IntegrationTests/Tests/ValuesTests.cs
+++ WebApiTestServer/WebApiTestServer.Api.IntegrationTests/Tests/ValuesTests.cs
@@ -1,17 +1,22 @@
 ﻿namespace WebApiTestServer.Api.IntegrationTests.Tests
 {
+    using Ploeh.AutoFixture.Xunit2;
+
     using WebApiTestServer.Api.IntegrationTests.Models;
 
     using Xunit;
 
-    public class Foo
+    public class ValuesTests
     {
         [Theory]
+        [AutoData]
         public void Test(MyTestServerFactory testServerFactory)
         {
             using (var serverFactory = testServerFactory.Create())
-            {               
+            {
+                var values = serverFactory.HttpClient.GetAsync("/api/values").Result.AsCollection<int>();
+                Assert.Equal(new[] { 1, 2, 3 }, values);
             }
         }
-    }
+    }    
 }
---
+++ WebApiTestServer/WebApiTestServer/HttpResponseMessageExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace WebApiTestServer
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Net.Http;    
+
+    using Newtonsoft.Json;
+
+    public static class HttpResponseMessageExtensions
+    {
+        public static TData As<TData>(this HttpResponseMessage message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+        
+            var responseBody = message.Content.ReadAsStringAsync().Result;
+            if (string.IsNullOrWhiteSpace(responseBody))
+            {
+                return default(TData);
+            }
+
+            return JsonConvert.DeserializeObject<TData>(responseBody);
+        }
+
+        public static IEnumerable<TData> AsCollection<TData>(this HttpResponseMessage message)
+        {
+            return As<IEnumerable<TData>>(message);
+        }
+    }
+}
